66b09c1955d84ad8837d98f329590cf8d0170054
oscar <oscarrodrigomez22@gmail.com>
2024-04-06 17:40:19 -0500

    Realización Pantalla Principal

--- app/src/main/java/com/example/taller2/MainActivity.kt
+++ app/src/main/java/com/example/taller2/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.taller2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val contactsButton = findViewById<ImageButton>(R.id.contactsButton)
+        val cameraButton = findViewById<ImageButton>(R.id.cameraButton)
+        val mapaButton = findViewById<ImageButton>(R.id.mapaButton)
+
+        contactsButton.setOnClickListener {
+            val intent = Intent(
+                this,
+                ContactosActivity::class.java
+            )
+            startActivity(intent)
+        }
+
+        cameraButton.setOnClickListener {
+            val intent = Intent(
+                this,
+                CamaraActivity::class.java
+            )
+            startActivity(intent)
+        }
     }
 }
